3c9e4bb2efc0bacda4eaaf8fc524f9aedff1c817
Abdiaziz Weheliye <aweheliy@ucsd.edu>
2020-02-20 19:15:48 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Abdiaziz Weheliye on 2/20/20.
+//  Copyright © 2020 Abdiaziz Weheliye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
