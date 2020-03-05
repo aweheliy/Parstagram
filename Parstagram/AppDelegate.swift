60b7f32b927fab116beb2e3c00a3dfb3b257b760
Abdiaziz Weheliye <aweheliy@ucsd.edu>
2020-03-04 19:28:24 -0800

    part 2 Parstagram

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -16,17 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        Parse.initialize(
-                 with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
-                     configuration.applicationId = "Parstagram"
-                     configuration.server = "https://fathomless-lake-76915.herokuapp.com/parse"
-                 })
-             )
         
         
         
         return true
     }
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Abdiaziz Weheliye. All rights reserved.
 //
 
+import Parse
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -17,7 +18,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        
+        Parse.initialize(
+                 with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                     configuration.applicationId = "Parstagram"
+                     configuration.server = "https://fathomless-lake-76915.herokuapp.com/parse"
+                 })
+             )
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            self.window = UIWindow(windowScene: windowScene)
+            self.window?.rootViewController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            self.window?.makeKeyAndVisible()
+
+            
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,7 +65,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+    
+   
 
 
 }
+
+
 
